b28b84dcd8442c716885b59d2bae4247ae6e0904
Deba <debaosuidecl@gmail.com>
2019-06-21 23:05:15 +0100

    updated db credentials

--- setdb.php
+++ setdb.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'mrziad');
+$conn = mysqli_connect('r42ii9gualwp7i1y.chr7pe7iynqr.eu-west-1.rds.amazonaws.com', 'pif7yquy2rprrawz', 'w31rgodyjxza96ys', 'rymmy2sz9p65xc6f');
 //check connection
 
 if(!$conn){
