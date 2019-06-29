b1241886182549b217c84f529d79327b3a272429
Deba <debaosuidecl@gmail.com>
2019-06-29 11:53:43 +0100

    enabling sendgrid

--- sendquotationtoclient.php
+++ sendquotationtoclient.php
@@ -1,28 +1,23 @@
 <?php
 
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
+require("./sendgrid-php/sendgrid-php.php");// Comment out the above line if not using Composer
 // require("<PATH TO>/sendgrid-php.php");
 // If not using Composer, uncomment the above line and
 // download sendgrid-php.zip from the latest release here,
 // replacing <PATH TO> with the path to the sendgrid-php.php file,
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("oosuide@yahoo.com", "Example User");
-$email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("debaosuidecl@gmail.com", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong> <a>CLICK ME</a>"
-);
-$sendgrid = new \SendGrid(getenv('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
-'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
-}
+
+$from = new SendGrid\Email(null, "test@example.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "debaosuidecl@gmail.com");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
