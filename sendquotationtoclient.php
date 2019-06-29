b7fbf965b211a9ccaecc46d4c0d791c6ede54209
Deba <debaosuidecl@gmail.com>
2019-06-29 18:12:51 +0100

    change from

--- sendquotationtoclient.php
+++ sendquotationtoclient.php
@@ -5,22 +5,25 @@
     // You need this API_KEY created on the Sendgrid website.
     // require 'lib/SendGrid/Email.php';
 	// include_once('credentials.php');
-	
-	$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("app137468473@heroku.com", "Example User");
+$API_KEY = "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"; 	
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("debaosuidecl@gmail.com", "Example User");
 $email->setSubject("Sending with Twilio SendGrid is Fun");
-$email->addTo("debaosuidecl@gmail.com", "Example User");
+$email->addTo("degrapheng@gmail.com", "Example User");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
+// $email->addContent(
+//     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+// );
+$sendgrid = new \SendGrid($API_KEY);
+if($sendgrid->send($email)){
+    echo "Email sent successfully";
 }
+// try {
+//     $response = $sendgrid->send($email);
+//     print $response->statusCode() . "\n";
+//     print_r($response->headers());
+//     print $response->body() . "\n";
+// } catch (Exception $e) {
+//     echo 'Caught exception: '. $e->getMessage() ."\n";
+// }
 ?>
